28f2034ee694fb1bd1d9a8bdb9e31f8b08bc97b1
illarramendiisabella-hub <illarramendi.isabella@correo.unimet.edu.ve>
2025-11-20 19:11:13 -0400

    Update simulaciondia.py

--- simulaciondia.py
+++ simulaciondia.py
@@ -1,4 +1,8 @@
 import random 
+from inventario import Inventario
+from menu import Menu
+from ingredientes import Ingrediente
+from hotdog import Hotdog
 
 class SimulacionDia:
     """Clase encargada de la simulación de un día de ventas."""
@@ -127,4 +131,5 @@ class SimulacionDia:
         print(f"7. Ingredientes que causaron que el cliente se marchara: {ing_marcha_str}")
 
         # 8. Total de acompañantes vendidos
+
         print(f"8. Total de acompañantes vendidos: {self.resultados['total_acompanantes_vendidos']}")
